75ddb4777bffc7b071ea8a1effeb861584e1965a
lalysingh <lalysingh1996@hotmail.fr>
2019-09-25 11:50:35 +0200

    active button

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,61 +28,54 @@
     localStorage.autoclick = localStorage.getItem("autoclick") || 0;
     $score.innerHTML = parseInt(localStorage.score);
 
-    let timer = 20;
-    let clickcount = 0;
-    function activeBtn (btn, price){
-        if (localStorage.score >= price) {
+    // Functions
+
+    //toggle means active and desactive one button
+    //fonction va aller chercher l'index qu'on lui donne du bouton
+    //pour chaque index on active ou desactive le bouton
+    function toggleOneBtn(btnindex) {
+        const $btn = btns[btnindex];
+        // recupere l'element avec la valeur de l'index du tableau prices
+        const $price = prices[btnindex];
+        //recupere vraie valeur donc 
+        const price = parseInt($price.innerHTML);
+        // recupere valeur pour score et pas element
+        const score = parseInt(localStorage.score);
+        if (score >= price) {
             // si valeur de score plus grande que 10 alors active le btn
-            btn.removeAttribute("disabled");
+            $btn.removeAttribute("disabled");
         } else {
             //sinon desactive le bouton
-            btn.setAttribute("disabled", "true");
+            $btn.setAttribute("disabled", "true");
         }
     }
-    function decreaseTime (){
-        ti = ti-1;
-    }
-    function animateTime($time, ti){
-        // acceder a l'element time 
-        if (ti > 0){
-            setInterval(decreaseTime, 1000);
+    function toggleAllBtns() {
+        for (let btnindex in btns) {
+            toggleOneBtn(btnindex);
         }
     }
+
+    function addCookie() {
+        const quantityAdd = parseInt(localStorage.multiplier)*1;
+        const newScore = parseInt(localStorage.score) + quantityAdd;
+        $score.innerHTML = newScore;
+        localStorage.score = newScore;
+    }
+
+    
+    //Events
 
     $cookie.addEventListener("click", () => {
-        // ajoute a la valeur de score 1 dans object localstorage
-        localStorage.score++;
-        // va changer dans l'element score avec methode innerHTML = prends valeur de score
-        $score.innerHTML = localStorage.score;
-        activeBtn($btnmult, 10);
-        activeBtn($btnauto, 20);
-        activeBtn($btnbon, 30);
+        addCookie();
+        toggleAllBtns();
     });
 
-    $reset.addEventListener("click", () => { 
-        // supprimer la propriete score
-        localStorage.score = 0;
+    $reset.addEventListener("click", () => {
         $score.innerHTML = 0;
-        activeBtn($btnmult, 10);
-        activeBtn($btnauto, 20);
-        activeBtn($btnbon, 30);
-    });
-
-    // quand click sur btn mult une fois alors il retire 10 cookies de la variable
-    $btnmult.addEventListener("click", ()=> {
-        clickcount++;
-        time--;
-        console.log(time);
-        if (clickcount == 1){
-            localStorage.score = localStorage.score - 10;
-            $score.innerHTML = localStorage.score;
-            console.log(localStorage.score);
-        } else if (clickcount > 1){
-            localStorage.score = localStorage.score * 2;
-            $score.innerHTML = localStorage.score;
-            console.log(localStorage);
-
-        };
+        localStorage.score = 0;
+        localStorage.multiplier = 1;
+        localStorage.autoclick = 0;
+        toggleAllBtns();
     });
 
 })();
